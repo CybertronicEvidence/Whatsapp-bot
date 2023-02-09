1e360cef5e6fb87331a0345ac66f122d4d6e1655
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-02-09 01:42:38 +0100

    updated chat page

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -2,58 +2,43 @@ import {
     Configuration,
     OpenAIApi
 } from "openai";
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openAI = new OpenAIApi(configuration);
-export default async function (req, res) {
-    let reply = '';
-    const message = req.body.Body || '';
-    console.log(message);
-    try {
-        const response = await openAI.createCompletion({
-            model: "text-davinci-003",
-            prompt: req.body.Body,
-            max_tokens: 256,
-            temperature: 0.5,
-        });
-        reply = response.data.choices[0].text;
-    } catch (error) {
-        reply = 'There was an issue not caused by you'
-    }
 
-    // send response
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
+const message = req.body.Body || '';
+
+const openaiCompletion = async (prompt = message) => {
+    const response = await openAI.createCompletion({
+        prompt,
+        max_tokens: 100,
+        n: 1,
+        stop: "",
+        temperature: 0.5,
+    }, {
+        headers: {
+            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+            "Content-Type": "application/json",
+        },
     });
-    res.end(reply.toString());
-    // res.status(200).json({ result: response.data.choices[0].text });
-    // console.log(response.data.choices[0].text)
+
+    return response.data;
 };
 
+export default async (req, res) => {
+    const { text } = req.query;
 
-// Note: This assumes you have already installed the openai package.
+    if (!text) {
+        return res.status(400).json({ error: "Text is required" });
+    }
 
-
-
-
-
-// export default async function handler(req, res) {
-//     const { message } = req.body.Body
-//     const response = await openAI.createCompletion({
-//         model: "text-davinci-003", // required
-//         prompt: `${message}`, // completion based on this
-//         temperature: 0.6, //
-//         n: 1,
-//         max_tokens: 50,
-//         // stop: "."
-//     });
-//     // res.status(200).json(response.data.choices[0].text)
-//     // console.log(response.data.choices[0].text)
-
-//     res.writeHead(200, {
-//         'Content-Type': 'application/json'
-//     });
-//     res.end(response.data.choices[0].text);
-// }
+    try {
+        const completion = await openaiCompletion(text);
+        return res.json({
+            result: completion.choices[0].text,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
